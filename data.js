44dd76343c7c8503d7430191d4da9839ac984b5b
justchin121 <justchin121@gmail.com>
2026-07-25 00:28:25 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,74 @@
+// Fixed Data
+const columnClasses = [
+    ["w0", "basic", "drug-no"],
+    ["w1f", "basic", "no-scroll"],
+    ["w1", "basic"],
+
+    ["w1", "pharmc"],
+    ["w2", "pharmc"],
+    ["w1", "pharmc"],
+    ["w1", "pharmc"],
+    
+    ["w3", "therap"],
+    ["w3", "therap"],
+    ["w1", "therap"],
+
+    ["w2", "practuse"],
+    ["w2", "practuse"],
+    ["w2", "practuse"],
+    ["w2", "practuse"],
+
+    ["w2", "practsafe"],
+    ["w2", "practsafe"],
+    ["w2", "practsafe"],
+    ["w2", "practsafe"],
+    ["w2", "practsafe"],
+    ["w2", "practsafe"],
+    ["w2", "practsafe"],
+    ["w2", "practsafe"],
+
+    ["w1", "pk"],
+    ["w1", "pk"],
+    ["w1", "pk"],
+    ["w1", "pk"],
+
+    ["w1", "basic"], 
+    ["w1", "basic"],
+    ["w1", "basic"],
+];
+
+/* Original Sheet Column Indices
+0   = Gen Name
+1   = Brand Name
+
+2   = Pharmacological Class 1
+3   = Pharmacological Class 2
+4   = Add. Pharmacological Class
+
+5   = Therapuetic Class
+6   = Indications
+7   = Affected Systems
+
+8   = Route of Administration
+9   = Dosage
+10  = Counseling Points
+11  = Legal Classification
+
+12  = ADR (Common)
+13  = ADR (Serious) 
+14  = Contraindications
+15  = Precautions
+16  = Drug Interactions
+17  = Food Interactions
+18  = Special Populations
+19  = Monitoring
+
+20  = Absorption
+21  = Distribution
+22  = Metabolism
+23  = Elimination
+
+24  = Notes
+25  = Tags
+26  = Course Encounter
+*/
---
+++ project.js
@@ -0,0 +1,96 @@
+// Initialization
+const sheetAPIKey = "AIzaSyDFybl2Kxp4RMuCiT1fkTYJUImPfgK8s7g"; 
+const sheetId = "1bh0Jr6vpKCfaO8c30WHx2nat-NidhI7qNJ963TFgSpY";
+const sheetRange = encodeURIComponent("Drugs!A:AB");
+const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetRange}?key=${sheetAPIKey}`;
+
+const HTML_thead = document.querySelector("thead");
+const HTML_tbody = document.querySelector("tbody");
+const HTML_searchDrug = document.querySelector(".search-drug.search-input");
+const HTML_searchGen = document.querySelector(".search-gen.search-input");
+
+// Update Table
+function updateTable(data) {
+    HTML_thead.innerHTML = "";
+    HTML_tbody.innerHTML = "";
+    let realIndex = 0;
+
+    data.forEach(addRow);
+    function addRow(row, index) {
+        const tr = document.createElement("tr");
+        const searchDrugText = HTML_searchDrug.value;
+        const searchGenText = HTML_searchGen.value;
+        const regex_searchDrugText = new RegExp(searchDrugText, "gi");
+        const regex_searchGenText = new RegExp(searchGenText, "gi");
+
+        if (index !== 0 && !row[0].toLowerCase().includes(searchDrugText.toLowerCase())) {return};
+        if (index !== 0 && !row.toString().toLowerCase().includes(searchGenText.toLowerCase())) {return};
+
+        const type = (index === 0) ? "th" : "td";
+        const rowContent = (index === 0) ? ["No.", ...row] : [realIndex + 1, ...row];
+        if (index !== 0) {realIndex++};
+
+        rowContent.forEach(addCell);
+        function addCell(value, index) {
+            if (!config.enabledColumns[index]) {return}
+
+            let origIndex = index;
+            if (origIndex === 1 && searchDrugText.length > 1) {
+                value = value.replaceAll(regex_searchDrugText, match => 
+                `<span class="textFound">${match}</span>`);
+            }
+            if (origIndex !== 0 && searchGenText.length > 1) {
+                value = value.replaceAll(regex_searchGenText, match => 
+                `<span class="textFound">${match}</span>`);
+            }
+
+            const cell = document.createElement(type); 
+
+            cell.classList.add(...columnClasses[index]);
+            
+            cell.innerHTML = value ?? "";
+            tr.appendChild(cell);
+        }
+
+        if (index === 0) {
+            HTML_thead.appendChild(tr);
+        } else {
+            HTML_tbody.appendChild(tr);
+        }
+    }
+}
+
+function run(){
+    fetch(sheetUrl)
+        .then(response => {
+            console.log("HTTP Status:", response.status);
+            return response.json();
+        })
+
+        .then(data => {
+
+            console.log("API Response:", data);
+
+            if (data.error) {
+                console.error("Google API Error:", data.error.message);
+                return;
+            }
+
+            if (!data.values) {
+                console.error("No values were returned.");
+                return;
+            }
+
+            updateTable(data.values);
+            console.log(data);
+
+        })
+        
+        .catch(error => {
+            console.error("Fetch Error:", error);
+        });
+}
+
+// HTML Events
+HTML_searchDrug.addEventListener('input', (event) => {run()});
+HTML_searchGen.addEventListener('input', (event) => {run()});
